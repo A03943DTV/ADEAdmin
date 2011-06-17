3ab0a1d00f0c54211ca5efcc71567bd5b531f245
Meiyazhagan <meiyazhagan_cse@yahoo.co.in>
2011-06-17 20:47:02 +0530

    Change-Id: I0dbe30ae43370f6f02d134dfe7835d86331cf990

---
+++ AdminController/src/com/directv/adminuserinterface/shared/validator/UserValidator.java
@@ -0,0 +1,36 @@
+/*
+ * Author : Meiy
+ */
+package com.directv.adminuserinterface.shared.validator;
+
+import com.directv.adminuserinterface.shared.User;
+import com.directv.adminuserinterface.util.AdminException;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Class UserValidator.
+ */
+public class UserValidator {
+
+	/**
+	 * Validate.
+	 *
+	 * @param user the user
+	 * @throws AdminException the admin exception
+	 */
+	public static void validate(User user) throws AdminException {
+
+		if (user.getFirstName() == null || user.getFirstName().equals("")) {
+			throw new AdminException("FirstName is required");
+		}
+		if (user.getLastName() == null || user.getLastName().equals("")) {
+			throw new AdminException("LastName is required");
+		}
+		if (user.getUserId() == null || user.getUserId().equals("")) {
+			throw new AdminException("UserId is required");
+		}
+		if (user.getLocation() == null || user.getLocation().equals("")) {
+			throw new AdminException("Location is required");
+		}
+	}
+}
